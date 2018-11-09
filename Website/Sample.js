cd69f464942ec93a963b37fd7c2c80e7ff65c8f7
Aaron Josh <me@aaronjosh.com>
2018-11-09 09:52:16 -0600

    Update Sample.js

--- Website/Sample.js
+++ Website/Sample.js
@@ -2,11 +2,11 @@
 $.ajax
 ({
   type: "GET",
-  url: https://api.mysportsfeeds.com/v1.0/pull/nfl/2017-regular/cumulative_player_stats.json?player=smith,
+  url: "https://api.mysportsfeeds.com/v1.0/pull/nfl/2017-regular/cumulative_player_stats.json?player=smith",
   dataType: 'json',
   async: false,
   headers: {
-    "Authorization": "Basic " + btoa(389c9cb2-7e84-4cb4-a0ec-b2fbaa + ":" + Apr231991)
+    "Authorization": "Basic " + btoa("389c9cb2-7e84-4cb4-a0ec-b2fbaa:Apr231991")
   },
   data: '{ "comment" }',
   success: function (){
